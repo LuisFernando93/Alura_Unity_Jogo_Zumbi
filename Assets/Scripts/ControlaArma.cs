a7199b5f6f50238e7dcd4ec60d405c3685e7cb6c
Luis Fernando Pasquinelli Amaral de Abreu <luispasquinelli@hotmail.com>
2021-07-02 21:55:39 -0300

    O jogador agora pode atirar com o botao esquerdo do mouse

---
+++ Assets/Scripts/ControlaArma.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlaArma : MonoBehaviour
+{
+
+    public GameObject Projetil;
+    public GameObject CanoDaArma;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Projetil, CanoDaArma.transform.position, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour
+{
+
+    public float Velocidade = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward * Velocidade * Time.deltaTime);
+    }
+}
